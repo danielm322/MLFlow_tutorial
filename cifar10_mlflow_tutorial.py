9aa59b1b2a1009170d87f5c9163dab8276b5c905
dm274516 <daniel-alfonso.montoyavasquez@cea.fr>
2024-08-08 16:06:34 +0200

    Added mlflow capabilities

--- cifar10_mlflow_tutorial.py
+++ cifar10_mlflow_tutorial.py
@@ -20,10 +20,11 @@ import mlflow
 
 # ### Hyperparameters definition
 
-batch_size = 64
-learning_rate = 0.001
-num_epochs = 155
-num_classes = 10
+BATCH_SIZE = 64
+LEARNING_RATE = 0.005
+NUM_EPOCHS = 20
+DROPOUT_RATE = 0.1
+EXPERIMENT_NAME = "CIFAR10_Mlflow_tutorial"
 
 # # Dataset Loading
 
@@ -48,11 +49,11 @@ train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_s
 
 # Create train loader
 train_loader = DataLoader(dataset=train_dataset,
-                          batch_size=batch_size,
+                          batch_size=BATCH_SIZE,
                           shuffle=True)
 # Create val loader
 val_loader = DataLoader(dataset=val_dataset,
-                        batch_size=batch_size,
+                        batch_size=BATCH_SIZE,
                         shuffle=False)
 
 # Create Testing dataset
@@ -63,7 +64,7 @@ test_dataset = torchvision.datasets.CIFAR10(root='./data',
 
 # Create test loader
 test_loader = DataLoader(dataset=test_dataset,
-                         batch_size=batch_size,
+                         batch_size=BATCH_SIZE,
                          shuffle=False)
 
 
@@ -89,7 +90,7 @@ class Custom_CNN(nn.Module):
 
         self.fc_layer_1 = nn.Linear(2 * 2 * 256, 512)
 
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(DROPOUT_RATE)
 
         self.fc_layer_2 = nn.Linear(512, 10)
 
@@ -136,7 +137,7 @@ criterion = nn.CrossEntropyLoss()
 # ### Optimizer definition
 
 # Stochastic Gradient Descent (SGD) optimizer
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.005, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, weight_decay=0.005, momentum=0.9)
 
 # ### Total number of iterations
 
@@ -145,115 +146,139 @@ total_step = len(train_loader)
 
 # ### Device selection (CPU/GPU) for training
 
-# In[14]:
-
-
 # Device will determine whether to run the training on GPU or CPU.
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # ### Model training with evaluation
-
-# In[19]:
-
 
 # Model training with evaluation
 for name, param in model.named_parameters():
     if param.requires_grad:
         print(name, param.data.shape)
 
-for epoch in range(num_epochs):
+###############################################################
+# Training
+###############################################################
 
-    # Set the model to training mode
-    model.train()
-    running_loss = 0.0
-    correct = 0
-    total = 0
+# Mlflow parameters
+experiment_name = EXPERIMENT_NAME
+mlflow.set_tracking_uri("http://10.8.33.50:5050")
+existing_exp = mlflow.get_experiment_by_name(experiment_name)
+if not existing_exp:
+    mlflow.create_experiment(
+        name=experiment_name,
+    )
+experiment = mlflow.set_experiment(experiment_name=experiment_name)
+mlflow_run_name = f"lr_{LEARNING_RATE}_dropout_{int(DROPOUT_RATE*10)}"
+with mlflow.start_run(
+        experiment_id=experiment.experiment_id,
+        run_name=mlflow_run_name
+) as run:
 
-    for i, data in enumerate(train_loader):
-        inputs, labels = data
-        optimizer.zero_grad()
+    # Log parameters to mlflow
+    mlflow.log_param("learning_rate", LEARNING_RATE)
+    mlflow.log_param("batch_size", BATCH_SIZE)
+    mlflow.log_param("num_epochs", NUM_EPOCHS)
+    mlflow.log_param("dropout_rate", DROPOUT_RATE)
 
-        # Forward pass
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
+    for epoch in range(NUM_EPOCHS):
 
-        # Backward pass
-        loss.backward()
-        optimizer.step()
+        # Set the model to training mode
+        model.train()
+        running_loss = 0.0
+        correct = 0
+        total = 0
 
-        running_loss += loss.item()
-
-        # For accuracy calculation
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    # Calculate the average training loss for this epoch
-    avg_training_loss = running_loss / len(train_loader)
-    training_accuracy = 100 * correct / total
-
-    # Evaluate the model on the validation set
-    model.eval()
-    val_loss = 0.0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in val_loader:
+        for i, data in enumerate(train_loader):
             inputs, labels = data
+            optimizer.zero_grad()
+
+            # Forward pass
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-            val_loss += loss.item()
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
 
             # For accuracy calculation
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    avg_val_loss = val_loss / len(val_loader)
-    val_accuracy = 100 * correct / total
+        # Calculate the average training loss for this epoch
+        avg_training_loss = running_loss / len(train_loader)
+        training_accuracy = 100 * correct / total
 
-    print(f'Epoch [{epoch + 1}/{num_epochs}], '
-          f'Training Loss: {avg_training_loss:.4f}, '
-          f'Training Accuracy: {training_accuracy:.2f}%, '
-          f'Validation Loss: {avg_val_loss:.4f}, '
-          f'Validation Accuracy: {val_accuracy:.2f}%')
+        # Evaluate the model on the validation set
+        model.eval()
+        val_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in val_loader:
+                inputs, labels = data
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
 
-print('Training finished!')
+                # For accuracy calculation
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-# ### Plot the Accuracy and Loss Values
-#
+        avg_val_loss = val_loss / len(val_loader)
+        val_accuracy = 100 * correct / total
 
-# ### Save the trained model
+        print(f'Epoch [{epoch + 1}/{NUM_EPOCHS}], '
+              f'Training Loss: {avg_training_loss:.4f}, '
+              f'Training Accuracy: {training_accuracy:.2f}%, '
+              f'Validation Loss: {avg_val_loss:.4f}, '
+              f'Validation Accuracy: {val_accuracy:.2f}%')
 
-# In[20]:
+        mlflow.log_metric("train_loss", avg_training_loss, step=epoch)
+        mlflow.log_metric("train_accuracy", training_accuracy, step=epoch)
+        mlflow.log_metric("val_loss", avg_val_loss, step=epoch)
+        mlflow.log_metric("val_accuracy", val_accuracy, step=epoch)
+
+    print('Training finished!')
+
+    # Plot the Accuracy and Loss Values
+
+    # ### Save the trained model
 
 
-path = './cifar.pth'
-torch.save(model.state_dict(), path)
+    path = './cifar.pth'
+    torch.save(model.state_dict(), path)
 
-# ### Test the model
-
-# In[21]:
+    # ### Test the model
 
 
-model.eval()  # Set the model to evaluation mode
-test_loss = 0.0
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in test_loader:
-        inputs, labels = data
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        test_loss += loss.item()
+    model.eval()  # Set the model to evaluation mode
+    test_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            inputs, labels = data
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
 
-        # For accuracy calculation
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+            # For accuracy calculation
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-avg_test_loss = test_loss / len(test_loader)
-test_accuracy = 100 * correct / total
+    avg_test_loss = test_loss / len(test_loader)
+    test_accuracy = 100 * correct / total
 
-print(f'Test Loss: {avg_test_loss:.4f}, '
-      f'Test Accuracy: {test_accuracy:.2f}%')
+    print(f'Final Test Loss: {avg_test_loss:.4f}, '
+          f'Final Test Accuracy: {test_accuracy:.2f}%')
+
+    mlflow.log_metric("final_test_loss", avg_test_loss, step=0)
+    mlflow.log_metric("final_test_accuracy", test_accuracy, step=0)
+
+    mlflow.end_run()
